37540241d959b13238cac9df7d5c7cc0ea03dfc2
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-02-03 00:03:53 -0300

    linking functionsinserting functions for the transaction class

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
- const Modal = {
+const Modal = {
     open(){
         document
             .querySelector('.modal-overlay')
@@ -11,4 +11,39 @@
             .classList
             .remove('active')
     }
+}
+
+const transactions = [
+    {
+        id:1,
+        description: 'Luz',
+        amount: -5000,
+        date:'23/01/2021',
+    },
+    {
+        id:2,
+        description: 'Website',
+        amount: 500000,
+        date:'23/01/2021',
+    },
+    {
+        id:3,
+        description: 'Internet',
+        amount: -2000,
+        date:'23/01/2021',
+    },
+]
+
+
+const Transaction = {
+    incomes(){
+
+    },
+    expenses(){ 
+        
+    },
+
+total(){
+
+}
 }
