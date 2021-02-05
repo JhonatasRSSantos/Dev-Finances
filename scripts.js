f7a9e147bcf0909c554cc3c813ee518060f6efd6
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-02-04 23:11:12 -0300

    initiating transaction actions

--- scripts.js
+++ scripts.js
@@ -15,22 +15,28 @@ const Modal = {
 
 const transactions = [
     {
-        id:1,
+        id: 1,
         description: 'Energia',
         amount: -50000,
         date:'23/01/2021',
     },
     {
-        id:2,
+        id: 2,
         description: 'Criação Website',
         amount: 500000,
         date:'23/01/2021',
     },
     {
-        id:3,
+        id: 3,
         description: 'Internet',
         amount: -20000,
         date:'23/01/2021',
+    },
+    {
+        id: 4,
+        description: 'App vendido',
+        amount: -200000,
+        date:'28/01/2021',
     },
 ]
 
@@ -48,16 +54,39 @@ total(){
 }
 
 const DOM = {
-    innerHTMLTransaction() {
+    transactionsContainer: document.querySelector('#data-table tbody')
+    addTransaction(Transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSClass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = utils.formatCurrency(transaction.amount)
+
         const html = `
-        <tr>
-            <td class="description">Energia</td>
-            <td class="expense">-R$ 500,00</td>
-            <td class="date">23/01/2021</td>
+        <td class="description">$(transaction.description)</td>
+        <td class="${CSSClass}">$(transaction.amount)</td>
+        <td class="date">$(transaction.date)</td>
         <td>
             <img src="./assets/minus.svg" alt="Remover Transação">
         </td>
-    </tr>
     `
+    return html;
     }
 }
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+// DOM.addTransaction(transactions[1]);
+// DOM.addTransaction(transactions[2]);
+// DOM.addTransaction(transactions[3]);
+
+transaction.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
