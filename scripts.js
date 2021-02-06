e49b45bf4d5ee8f07fc516e6a6be76216ff9aa18
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-02-05 23:26:08 -0300

    implementing javascript functions

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 const Modal = {
-    open(){
+    open() {
         document
             .querySelector('.modal-overlay')
             .classList
             .add('active')
     },
-    close(){
+
+    close() {                                                                                                                                                                                                                               
         document
             .querySelector('.modal-overlay')
             .classList
@@ -20,18 +21,21 @@ const transactions = [
         amount: -50000,
         date:'23/01/2021',
     },
+
     {
         id: 2,
         description: 'Criação Website',
         amount: 500000,
         date:'23/01/2021',
     },
+
     {
         id: 3,
         description: 'Internet',
         amount: -20000,
         date:'23/01/2021',
     },
+
     {
         id: 4,
         description: 'App vendido',
@@ -41,52 +45,53 @@ const transactions = [
 ]
 
 const Transaction = {
-    incomes(){
-
+    incomes() {
+        // + input
     },
-    expenses(){ 
-        
+    expenses() { 
+        // + exit
     },
-
-total(){
-
-}
+    total() {
+        // inputs - exits
+    }
 }
 
 const DOM = {
-    transactionsContainer: document.querySelector('#data-table tbody')
+
+    transactionsContainer: document.querySelector('#data-table tbody'),
     addTransaction(Transaction, index) {
         const tr = document.createElement('tr')
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
 
         DOM.transactionsContainer.appendChild(tr)
     },
+
     innerHTMLTransaction(transaction) {
-        const CSSClass = transaction.amount > 0 ? "income" : "expense"
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = utils.formatCurrency(transaction.amount)
 
         const html = `
-        <td class="description">$(transaction.description)</td>
-        <td class="${CSSClass}">$(transaction.amount)</td>
-        <td class="date">$(transaction.date)</td>
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
         <td>
             <img src="./assets/minus.svg" alt="Remover Transação">
         </td>
-    `
-    return html;
+        `
+
+        return html
+
     }
 }
+
 const Utils = {
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
     }
 }
 
-// DOM.addTransaction(transactions[1]);
-// DOM.addTransaction(transactions[2]);
-// DOM.addTransaction(transactions[3]);
-
 transaction.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
+        DOM.addTransaction(transaction)
+
 })
