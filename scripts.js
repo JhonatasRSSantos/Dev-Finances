4ec8779a956e21d7e944d9f8d1fac841b132183a
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-02-07 22:59:13 -0300

    javascripts functions for value changes in values

--- scripts.js
+++ scripts.js
@@ -65,14 +65,15 @@ const DOM = {
 
         DOM.transactionsContainer.appendChild(tr)
     },
+    
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
-        const amount = utils.formatCurrency(transaction.amount)
+        const amount = Utils.formatCurrency(transaction.amount)
 
         const html = `
         <td class="description">${transaction.description}</td>
-        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
             <img src="./assets/minus.svg" alt="Remover Transação">
@@ -81,12 +82,26 @@ const DOM = {
 
         return html
 
+    },
+
+    updateBalance(){
+        
     }
 }
 
 const Utils = {
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g,"")
+
+        Number(value) / 100
+
+        value = value.toLocaleString("pt-BR," {
+            style: "currency",
+            currency: "BRL"
+        })
+        return signal + value
     }
 }
 
